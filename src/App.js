94ab50e8c7a4f7778b79df81b9cec3ae448238e2
Anthony McLean <anthony@192.168.1.104>
2022-04-20 18:34:35 +0100

    sent refresh

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         setSentEmailList(response.data.reverse())
         setTotalEmailsSent(response.data.length || 0)
         setEmailJustSent(false)
-        console.log('app .get just ran')
+        console.log(response.data)
     })
 }, [emailJustSent])
 
